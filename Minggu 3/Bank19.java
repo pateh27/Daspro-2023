f21da44b111f9be7039490efd008be7b73991118
pateh27 <fathzfl@gmail.com>
2023-09-11 16:15:32 +0700

    bank

--- Minggu 3/Bank19.java
+++ Minggu 3/Bank19.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Bank19 {
-    public static void main(String[] args);{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int jml_tabungan_awal, lama_menabung;
         double prosentase_bunga =0.02, bunga, jml_tabungan_akhir;
         System.out.println("masukkan jumlah tabungan awal anda");
@@ -8,7 +11,7 @@ public class Bank19 {
         lama_menabung= input.nextInt();
         bunga= lama_menabung*prosentase_bunga*jml_tabungan_awal;
         jml_tabungan_akhir=bunga+jml_tabungan_awal;
-        System.out.println ("Jumlah tabungan akhir anda adalah " +jml_tabungan_akhir)
+        System.out.println ("Jumlah tabungan akhir anda adalah " +jml_tabungan_akhir);
 
     }
 
