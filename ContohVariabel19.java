85e29c40c38d3946b47bdd24adaef038abfb626f
pateh27 <fathzfl@gmail.com>
2023-09-11 13:09:09 +0700

    dimana kamu

---
+++ ContohVariabel19.java
@@ -0,0 +1,13 @@
+public class ContohVariabelNoAbsen {
+    public static void main(String[] args) {
+        String salahSatuHobbySayaAdalah = "bermain petak umpet";
+        boolean isPandai = true;
+        char JenisKelamin = 'L';
+        byte umurSayaSekarang = 19;
+        double $ipk = 3.24,tinggi = 1.70;
+        System.out.println(salahSatuHobbySayaAdalah);
+        System.out.println("JenisKelamin: " + JenisKelamin);
+        System.out.println("umurku saat ini: " + umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s" , $ipk, tinggi));
+    }
+} 
---
+++ Jobsheet2.java
@@ -0,0 +1,6 @@
+public class Jobsheet2 {
+    public static void main(String[] args) {
+        System.out.println("Tugas Jobsheet2");
+        
+    }
+}
