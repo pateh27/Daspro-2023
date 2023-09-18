9b2cf51d0ad5d82692217dd1b81035c52e433c69
pateh27 <fathzfl@gmail.com>
2023-09-18 12:06:37 +0700

    asd

--- Mnggu 4/Lingkaran19.java
+++ Mnggu 4/Lingkaran19.java
@@ -8,14 +8,14 @@ public interface Lingkaran19 {
     public static void main(String[] args) {
      Scanner sc =new Scanner(System.in);
      
-     int jariJari;
+     int r;
      double keliling, luas;
 
      System.out.println("masukkan jari-jari lingkaran: ");
-     jariJari=sc.nextInt();
+     r=sc.nextInt();
 
-     keliling= 2*3.14*jariJari;
-     luas=3.14*jariJari*jariJari;
+     keliling= 2*3.14*r;
+     luas=3.14*r*r;
 
      System.out.println(keliling);
      System.out.println(luas);
