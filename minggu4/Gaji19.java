b65306c0a3d7bd08b7c660ddaa2367b039ac3864
pateh27 <fathzfl@gmail.com>
2023-09-18 13:56:54 +0700

    belajar

--- minggu4/Gaji19.java
+++ minggu4/Gaji19.java
@@ -12,10 +12,11 @@ public class Gaji19 {
         System.out.println("Masukkan Jumlah Hari Masuk Kerja Anda");
         jmlMasuk=input.nextInt();
         System.out.println("Masukkan Jumlah Hari Tidak Masuk Kerja Anda");
+        JmlTdkMasuk=input.nextInt();
 
         TotGaji=(jmlMasuk*gaji)-(JmlTdkMasuk*potGaji);
 
         System.out.println("Gaji yang anda terima adalah " +TotGaji);
-        
+
     }
 }
