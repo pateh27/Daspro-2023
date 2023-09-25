cb8962a7aaf4bd9ccb8b7799e6c3bcfe2203a7f8
pateh27 <fathzfl@gmail.com>
2023-09-25 12:32:04 +0700

    minggu 5

--- minggu5/PemilihanPercobaan119.java
+++ minggu5/PemilihanPercobaan119.java
@@ -6,10 +6,7 @@ public class PemilihanPercobaan119 {
         System.out.println("Masukkan angka: ");
         int angka = input00.nextInt();
 
-            if (angka % 2 == 0)
-                System.out.println("Angka "+angka+" bilangan genap");
-            else
-                System.out.println("Angka "+angka+"bilangan ganjil");
+        System.out.println("Angka "+angka+ " bilangan " +(angka%2 == 0 ? " genap " : " ganjil "));
             
     }
 }
