0d2cedd62f68bd8fd5f6e90ded73aef23178d17d
pateh27 <fathzfl@gmail.com>
2023-09-25 14:03:31 +0700

    after 2

--- minggu5/PemilihanPercobaan119.java
+++ minggu5/PemilihanPercobaan119.java
@@ -2,9 +2,9 @@ package minggu5;
 import java.util.Scanner;
 public class PemilihanPercobaan119 {
     public static void main(String[] args) {
-        Scanner input00 = new Scanner(System.in);
+        Scanner input19 = new Scanner(System.in);
         System.out.println("Masukkan angka: ");
-        int angka = input00.nextInt();
+        int angka = input19.nextInt();
 
         System.out.println("Angka "+angka+ " bilangan " +(angka%2 == 0 ? " genap " : " ganjil "));
             
--- minggu5/PemilihanPercobaan219.java
+++ minggu5/PemilihanPercobaan219.java
@@ -16,9 +16,42 @@ public class PemilihanPercobaan219 {
         float tugas = input19.nextFloat();
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+        System.out.println(total);
 
-        String massage = total < 65 ? "Remidi" : "Tidak remidi";
-
-        System.out.println("Nilai akhir = " +total+ " sehingga " + massage);
+        if (total >= 80 && total <= 100) {
+            System.out.println("Nilai huruf     : A");
+            System.out.println("Nilai setara    : 4");
+            System.out.println("Kategori nilai  : Sangat Baik");
+        }
+        else if (total >= 73 && total <= 80) {
+            System.out.println("Nilai huruf     : B+");
+            System.out.println("Nilai setara    : 3.5");
+            System.out.println("Kategori nilai  : Lebih dari Baik");
+        }
+        else if (total >= 65 && total <= 73) {
+            System.out.println("Nilai huruf     : B");
+            System.out.println("Nilai setara    : 3");
+            System.out.println("Kategori nilai  : Baik");
+        }
+        else if (total >= 60 && total <= 65) {
+            System.out.println("Nilai huruf     : C+");
+            System.out.println("Nilai setara    : 2,5");
+            System.out.println("Kategori nilai  : Lebih dari Cukup");
+        }
+        else if (total >= 50 && total <= 60) {
+            System.out.println("Nilai huruf     : C");
+            System.out.println("Nilai setara    : 2");
+            System.out.println("Kategori nilai  : Cukup");
+        }
+        else if (total >= 39 && total <= 50) {
+            System.out.println("Nilai huruf     : D");
+            System.out.println("Nilai setara    : 1");
+            System.out.println("Kategori nilai  : Kurang");
+        }
+        else {
+            System.out.println("Nilai huruf     : E");
+            System.out.println("Nilai setara    : 0");
+            System.out.println("Kategori nilai  : Gagal");
+        } 
     }
 }
