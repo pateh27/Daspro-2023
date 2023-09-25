82a02aede7727bb774ace147d8f12685ab919ad7
pateh27 <fathzfl@gmail.com>
2023-09-25 12:55:12 +0700

    percobaan 2 before

--- minggu5/PemilihanPercobaan219.java
+++ minggu5/PemilihanPercobaan219.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class PemilihanPercobaan219 {
     public static void main(String[] args) {
-        java.util.Scanner input00 = new Scanner(System.in);
+        java.util.Scanner input19 = new Scanner(System.in);
 
         System.out.print("Nilai Uas       : ");
-        float uas = input00.nextFloat();
+        float uas = input19.nextFloat();
         System.out.print("Nilai uts       : ");
-        float uts = input00.nextFloat();
+        float uts = input19.nextFloat();
         System.out.print("Nilai kuis      : ");
-        float kuis = input00.nextFloat();
+        float kuis = input19.nextFloat();
         System.out.print("Nilai tugas     : ");
-        float tugas = input00.nextFloat();
+        float tugas = input19.nextFloat();
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
 
