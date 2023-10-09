c26720143247acf2d8ef720e4345834d4f7aadfb
pateh27 <fathzfl@gmail.com>
2023-10-09 12:30:14 +0700

    modify

--- minggu7/ForKelipatan19.java
+++ minggu7/ForKelipatan19.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ForKelipatan19 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int kelipatan;
+        int kelipatan, rataRata;
         int jumlah =0, counter = 0;
 
         System.out.println("Masukkan bilangan kelipatan (1-9) : ");
@@ -16,7 +16,9 @@ public class ForKelipatan19 {
 
             }
         }
+        rataRata = jumlah / counter;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n" , kelipatan,rataRata);
     }   
 }
