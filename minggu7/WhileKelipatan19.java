f3e527d92025920a1b27d1f37559510cfb107133
pateh27 <fathzfl@gmail.com>
2023-10-09 13:51:33 +0700

    tugas

---
+++ minggu7/WhileKelipatan19.java
@@ -0,0 +1,25 @@
+package minggu7;
+import java.util.Scanner;
+
+public class WhileKelipatan19 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int kelipatan, rataRata;
+        int jumlah = 0, counter = 0, i = 1;
+
+        System.out.println("Masukkan bilangan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+        while (i <=50) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+             i++;   
+        } 
+        rataRata = (jumlah / counter);
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n" , kelipatan,rataRata);
+    }
+}
