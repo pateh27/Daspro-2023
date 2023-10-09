3f5718e0353d6525eeb5064eaa34b08e99734dea
pateh27 <fathzfl@gmail.com>
2023-10-09 15:49:26 +0700

    dowhile

---
+++ minggu7/DoWhileCuti19.java
@@ -0,0 +1,30 @@
+package minggu7;
+import java.util.Scanner;
+public class DoWhileCuti19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.println("Jatah cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.println("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.println("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0); 
+    }
+}
