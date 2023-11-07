c98b50ef110b8180486b8994746bbbfde75ce5d6
pateh27 <fathzfl@gmail.com>
2023-11-07 07:28:11 +0700

    try

--- minggu11/Star.java
+++ minggu11/Star.java
@@ -4,11 +4,11 @@ public class Star {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Masukkan nilai N = ");
+        System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
 
-        for (int i=0; i <= N; i++) {
-            System.out.println(N);
+        for (int i=1; i <= N; i++) {
+            System.out.print("*");
         }
     }
 }
