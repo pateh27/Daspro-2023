63ceb09607ec1b58f256274408b319bd91e98229
pateh27 <fathzfl@gmail.com>
2023-11-07 07:25:43 +0700

    first try

---
+++ minggu11/Star.java
@@ -0,0 +1,14 @@
+package minggu11;
+import java.util.Scanner;
+public class Star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan nilai N = ");
+        int N = sc.nextInt();
+
+        for (int i=0; i <= N; i++) {
+            System.out.println(N);
+        }
+    }
+}
