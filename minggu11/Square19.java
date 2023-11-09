fc5591e58a864ddb098424f29ae0c1b2bd1a224a
pateh27 <fathzfl@gmail.com>
2023-11-09 14:21:30 +0700

    modify

---
+++ minggu11/Square19.java
@@ -0,0 +1,15 @@
+package minggu11;
+import java.util.Scanner;
+public class Square19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan N = ");
+        int N= sc.nextInt();
+        for (int iOuter=0; iOuter<=N; iOuter++) {
+            for (int i = 0; i <= N; i++) {
+                System.out.print("*");     
+            }
+           System.out.println();
+        }
+    }
+}
