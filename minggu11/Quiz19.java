cea30997037819a08bd85793a8a5b870c521a789
pateh27 <fathzfl@gmail.com>
2023-11-12 20:21:40 +0700

    modify quiz

--- minggu11/Quiz19.java
+++ minggu11/Quiz19.java
@@ -4,20 +4,25 @@ import java.util.Random;
 public class Quiz19 {
     public static void main(String[] args) {
         Random random = new Random();
-        Scanner sc = new Scanner(System.in);
-
+        Scanner input = new Scanner(System.in);
         char menu ='y';
         do {
-            int number = random.nextInt(10) + 1;
-            boolean succes = false;
+            int number = random.nextInt(10)+1;
+            boolean success = false;
             do {
-                System.out.print("Tebak angka(1-10): ");
-                int answer = sc.nextInt();
-                sc.nextLine();
-                succes=(answer == number);
-            } while (!succes);
-                System.out.println("Apakah anda ingin mengulang permainan (Y/n)?");
-                menu = sc.nextLine().charAt(0);
-        } while (menu =='y' || menu == 'Y');
+                System.out.println("Tebak angka (1-10) : ");
+                int answer = input.nextInt();
+                input.nextLine();
+                if (answer > number) {
+                    System.out.println("Lebih kecil dari ");
+                } else if (answer < number) {
+                    System.out.println("Lebih besar dari ");
+                }
+                success = (answer==number);
+            } while (!success);
+            System.out.println("Apakah anda ingin mengulang permainan (Y/y)?");
+            menu = input.nextLine().charAt(0);
+        } while (menu=='y' || menu=='Y');
+        System.out.println("Terima kasih sudah bermain");
     }
 }
