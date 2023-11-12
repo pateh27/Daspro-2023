154ef1569383c11267f3ec5f45f12aa4ed2fbc05
pateh27 <fathzfl@gmail.com>
2023-11-12 20:30:43 +0700

    modify perocbaan 5

--- minggu11/NestedLoop_2341720119.java
+++ minggu11/NestedLoop_2341720119.java
@@ -3,22 +3,35 @@ import java.util.Scanner;
 public class NestedLoop_2341720119 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        double [][] temps = new double [5][7];
+        double temps[][] = new double[5][7];
+        double rerata[] = new double [5];
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("Kota ke - " + (i+1));
-            for (int j = 0; j < temps [0].length; j++) {
-                System.out.println("Hari ke - " + (j+1) + ": ");
+            System.out.println("Kota ke-" + (i+1));
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j+1) + " : ");
                 temps [i][j] = sc.nextDouble();
+                rerata[i] += temps [i][j];
+            }
+        }
+
+        for (int i = 0; i < rerata.length; i++) {
+            rerata[i] /= 7;
+        }
+
+        for (double[] kota : temps) {
+            int i = 1;
+            System.out.print("Kota ke-"+i+" : ");
+            for (double hari : kota) {
+                System.out.print(hari + " ");
             }
             System.out.println();
+            i++;
         }
-        for (int i = 0; i < temps.length; i++) {
-            System.out.print("Kota ke - " + (i+1)+ ": ");
-            for (int j =0; j < temps [0].length; j++) {
-                System.out.print(temps[i][j] + " ");
-            }
-            System.out.println();
+
+        for (int i = 0; i < rerata.length; i++) {
+            System.out.println("Rata-rata kota ke-" + (i+1) + " : " + rerata[i]);
         }
+
     }
 }
