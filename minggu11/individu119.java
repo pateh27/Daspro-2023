277106ad055daf6206e29e653f31f8a62a08a5a5
pateh27 <fathzfl@gmail.com>
2023-11-12 21:54:19 +0700

    individu 1

--- minggu11/individu119.java
+++ minggu11/individu119.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class individu119 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+       Scanner input = new Scanner(System.in);
 
         System.out.print("Masukkan nilai N (minimal 3): ");
         int N = input.nextInt();
@@ -13,6 +13,17 @@ public class individu119 {
             System.out.println("Nilai N minimal harus 3 atau lebih.");
         } else {
             System.out.println("Segitiga Angka:");
+
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
         }
     }
 }
+
+
+
+
