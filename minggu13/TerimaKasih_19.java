f404ff6e81d73ce79049b09b418f0225b5366e23
pateh27 <fathzfl@gmail.com>
2023-11-20 13:57:31 +0700

    try

---
+++ minggu13/TerimaKasih_19.java
@@ -0,0 +1,16 @@
+package minggu13;
+
+public class TerimaKasih_19 {
+    public static void UcapanTerimakasih() {
+        System.out.println(" Thank you for being the best teacher in the world.\n "+"You inspired in me a love for learning and made me deel laike i could as you anyting");
+        }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+    public static void main(String[] args){
+        UcapanTerimakasih();
+        String ucapan = "Terima kasih pak ... bu .. Semoga sehat selalu";
+        UcapanTambahan(ucapan);
+        }
+
+    }   
